9a97770dbb61dfff83d3481a9a9c1af2159b7da2
beamaia <beamaia.work@gmail.com>
2023-05-06 20:59:58 -0300

    add space
    Co-Authored-By: Sophie Dilhon <51868657+AHalic@users.noreply.github.com>

--- create_topics.sh
+++ create_topics.sh
@@ -2,4 +2,5 @@ echo "Creating topics..."
 /kafka/bin/kafka-topics.sh --create --topic uvIndex --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
 /kafka/bin/kafka-topics.sh --create --topic temperature --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
 /kafka/bin/kafka-topics.sh --create --topic precipitationProbability --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
+/kafka/bin/kafka-topics.sh --create --topic beachHour --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
 /kafka/bin/kafka-topics.sh --create --topic beachDay --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
