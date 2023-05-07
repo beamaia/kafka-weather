7d62638169dbd3455e1fb9a8ab290fdf08abbd66
beamaia <beamaia.work@gmail.com>
2023-05-06 21:00:08 -0300

    add beach hour
    Co-Authored-By: Sophie Dilhon <51868657+AHalic@users.noreply.github.com>

--- delete_topics.sh
+++ delete_topics.sh
@@ -3,3 +3,4 @@ echo "Deleting topics..."
 /kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic temperature
 /kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic precipitationProbability  
 /kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachDay  
+/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachHour
