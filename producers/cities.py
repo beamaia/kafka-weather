a146181aa4e16663318089a372060bc557f080d9
AHalic <sophie.gama@aluno.ufes.br>
2023-05-12 22:38:01 -0300

    :recycle: muda arquivo de cidades
    Co-authored-by: Bea Maia <beamaia@users.noreply.github.com>

--- producers/cities.py
+++
@@ -1,7 +0,0 @@
-CITIES = {
-    'ubatuba': 'latitude=-23.43&longitude=-45.07',
-    'guarapari': 'latitude=-20.67&longitude=-40.50',
-    'porto_de_galinhas': 'latitude=-8.51&longitude=-35.00',
-    'balneario_camboriu': 'latitude=-26.99&longitude=-48.63',
-    'cabo_frio': 'latitude=-22.89&longitude=-42.03',
-}
