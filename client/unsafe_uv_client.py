9a6c98cc9be261a87a94cf1da4836aae657c3a24
AHalic <sophie.gama@aluno.ufes.br>
2023-05-12 22:55:16 -0300

    :sparkles: adiciona cidades

--- client/unsafe_uv_client.py
+++ client/unsafe_uv_client.py
@@ -2,20 +2,36 @@ from kafka import KafkaProducer, KafkaConsumer, TopicPartition
 import json
 import time
 import datetime
+import argparse
+import difflib
+
+CITIES = json.loads(open('assets/cities.json', 'r').read())
 
 class UnsafeUvClient:
     uv_topic = 'uvIndex'
 
-    def __init__(self):
+    def __init__(self, city):
         self.consumer = KafkaConsumer(bootstrap_servers='kafka:9092', auto_offset_reset='earliest', value_deserializer=lambda x: json.loads(x))
         self.consumer.subscribe(self.uv_topic)
+        
+        self._verify_city(city)
 
+    def _verify_city(self, city):
+        if not any(difflib.get_close_matches(city, CITIES, n=1, cutoff=0.8)):
+            raise Exception(f"City {city} not found")
+        else:
+            self.city = difflib.get_close_matches(city, CITIES, n=1, cutoff=0.8)[0]
+            print(f"City {city} found. Using {self.city} instead")
 
     def __get_partition_messages(self, partitions):
         messages = []
         for _, message_list in partitions.items():
             for message in message_list:   
                 msg_json = json.loads(message.value.replace("'", '"'))
+
+                if msg_json['local'] != self.city:
+                    continue
+
                 msg_json['hora'] = datetime.datetime.strptime(msg_json['hora'], '%Y-%m-%dT%H:%M')
                 messages.append(msg_json)
         return messages
@@ -76,6 +92,20 @@ class UnsafeUvClient:
             self.run()
             time.sleep(3600)
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Unsafe UV Client')
+    parser.add_argument('--run-once', action='store_true', help='Run only once')
+    parser.add_argument('--run-forever', action='store_true', help='Run forever', default=True)
+    parser.add_argument('--city', type=str, help='City to get UV index from', required=True)
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    client = UnsafeUvClient()
-    client.run_forever()
+    args = parse_args()
+
+    if args.run_once:
+        client = UnsafeUvClient(args.city)
+        client.run()
+    elif args.run_forever:
+        client = UnsafeUvClient(args.city)
+        client.run_forever()
