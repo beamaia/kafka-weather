dca8108b37fce920c386acbca4d13d7a1f3a8d80
beamaia <beamaia.work@gmail.com>
2023-07-02 19:58:28 -0300

    WIP: join info from same day
    Co-Authored-By: Sophie Dilhon <51868657+AHalic@users.noreply.github.com>

--- src/main/java/kafkaWeather/BeachDay.java
+++ src/main/java/kafkaWeather/BeachDay.java
@@ -49,7 +49,7 @@ class BeachDay{
 		
         
         Properties props = new Properties();
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-weather");
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-weather-beach-day");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092"); 
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -62,31 +62,78 @@ class BeachDay{
         final KStream<String, JsonNode> beachHour = builder.stream("beachHour", consumed);
 
         final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
-        final LocalDateTime now = LocalDateTime.now().withMinute(0).withSecond(0).withNano(0);
 
-        final TimeWindowedKStream<String, JsonNode> aggregatedStream = beachHour
-        .groupBy((key, value) -> value.get("hora").asText())
-        .windowedBy(TimeWindows.of(Duration.ofMinutes(5)))
+        KTable<String, JsonNode> aggregatedTable = beachHour
+        .filter((key, value) -> value.get("boaHora").asBoolean())
+        .selectKey((key, value) -> value.get("local").asText() + value.get("hora").asText().split("T")[0] + value.get("isDay").asText())
+        .groupByKey()
         .aggregate(
-                () -> new ArrayList<>(),
-                (key, value, aggregate) -> {
-                    aggregate.add(value);
-                    return aggregate;
-                },
-                Materialized.with(Serdes.String(), jsonSerde)
-        )
-        .toStream()
-        .flatMapValues((ValueMapper<List<JsonNode>, Iterable<JsonNode>>) aggregate -> aggregate)
-        .mapValues((ValueMapper<JsonNode, JsonNode>) value -> {
-            String local = value.get("local").asText();
-            String start = value.get("hora").asText();
-            String end = LocalDateTime.parse(value.get("hora").asText()).plusHours(1).withMinute(59).format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
+            () -> JsonNodeFactory.instance.objectNode(),
+            (key, value, aggregate) -> {
+                ObjectNode result = JsonNodeFactory.instance.objectNode();
+                result.put("local", value.get("local").asText());
+                result.put("boaHora", value.get("boaHora").asBoolean());
+                result.put("isDay", value.get("isDay").asBoolean());
+                
+                System.out.println("Aggregate" + aggregate.toString() + " Value" + value.toString());
+                if (aggregate.isEmpty()) {
+                    // Handle the case when aggregate is null
+                    // initialize JsonNode list
+                    List<JsonNode> list = new ArrayList<JsonNode>();
+                    
+                    // create temp JsonNode
+                    ObjectNode temp = JsonNodeFactory.instance.objectNode();
+                    temp.put("inicio", value.get("hora").asText());
+                    temp.put("fim", value.get("hora").asText());
 
-            return createEventJsonNode(local, start, end);
-        });
-        
+                    // add temp JsonNode to list
+                    list.add(temp);
+
+                    // add list to result
+                    result.put("intervalos", list.toString());
+                    return result;
+                }
+
+                // String jsonString = "[{\"inicio\":\"2023-07-03T15:00\",\"fim\":\"2023-07-03T15:00\"}]";
+
+                // ObjectMapper objectMapper = new ObjectMapper();
+                // JsonNode jsonNode = objectMapper.readTree(jsonString);
+                // List<JsonNode> list = new ArrayList<>();
+                // if (jsonNode.isArray()) {
+                //     Iterator<JsonNode> iterator = jsonNode.elements();
+                //     while (iterator.hasNext()) {
+                //         JsonNode element = iterator.next();
+                //         list.add(element);
+                //     }
+                // }
+
+                // Handle the case when aggregate is not null
+                // get intervalos from aggregate
+                List<JsonNode> list = new ArrayList<JsonNode>();
+                String intervalos = aggregate.get("intervalos").asText();
+                
+                System.out.println("Intervalo" + intervalos);
+
+                // create temp JsonNode
+                ObjectNode temp = JsonNodeFactory.instance.objectNode();
+                temp.put("inicio", value.get("hora").asText());
+                temp.put("fim", value.get("hora").asText());
+
+                // Get string from JsonNode
+                String tempString = temp.toString();
+
+                // add temp string to intervalos
+                intervalos = intervalos.substring(0, intervalos.length() - 1) + "," + tempString + "]";
+                
+                // update intervalos
+                result.put("intervalos", intervalos);
+                return result;
+            },
+            Materialized.with(stringSerde, jsonSerde)
+        );
+
         // To stream
-        KStream<String, JsonNode> joinedStream = aggregatedStream.toStream();
+        KStream<String, JsonNode> joinedStream = aggregatedTable.toStream();
 
         // Print the final events to the console
         joinedStream.foreach((key, value) -> System.out.println("Event: " + value + " Key: " + key));
@@ -96,18 +143,5 @@ class BeachDay{
 
 		KafkaStreams streams = new KafkaStreams(builder.build(), props);
 		streams.start();
-
-		
-		System.out.println("Stream criado");
-
-		// streams.close();
 	}
-
-    private static JsonNode createEventJsonNode(String city, String start, String end) {
-        ObjectNode eventNode = JsonNodeFactory.instance.objectNode();
-        eventNode.put("local", city);
-        eventNode.put("inicio", start);
-        eventNode.put("fim", end);
-        return eventNode;
-    }
 }  
