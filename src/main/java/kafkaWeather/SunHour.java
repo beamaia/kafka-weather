9284e1b80c267b12bf886a8485c20d0aabcec454
beamaia <beamaia.work@gmail.com>
2023-07-02 19:57:08 -0300

    change APPLICATION_ID_CONFIG
    Co-Authored-By: Sophie Dilhon <51868657+AHalic@users.noreply.github.com>

--- src/main/java/kafkaWeather/SunHour.java
+++ src/main/java/kafkaWeather/SunHour.java
@@ -29,7 +29,7 @@ import java.time.Duration;
 class SunHour{  
 	public static void main(String[] args){    
 		Properties props = new Properties();
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-weather");
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-weather-sun-hour");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092"); 
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
