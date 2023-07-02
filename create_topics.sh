7d6b165404fb658780aca178cac68f7643301f39
beamaia <beamaia.work@gmail.com>
2023-07-02 19:56:24 -0300

    add beachHourFiltered topic
    Co-Authored-By: Sophie Dilhon <51868657+AHalic@users.noreply.github.com>

--- create_topics.sh
+++ create_topics.sh
@@ -6,4 +6,6 @@ echo "Creating topics..."
 /kafka/bin/kafka-topics.sh --create --topic beachDay --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
 /kafka/bin/kafka-topics.sh --create --topic sunset --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
 /kafka/bin/kafka-topics.sh --create --topic sunrise --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
-/kafka/bin/kafka-topics.sh --create --topic sunHour --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
+/kafka/bin/kafka-topics.sh --create --topic sunHour --partitions 3 --replication-factor 1 --bootstrap-server kafka:
+
+/kafka/bin/kafka-topics.sh --create --topic beachHourFiltered --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
