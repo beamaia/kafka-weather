48b8532fea259dc36d7acdd3668a98e0f7b2c51f
beamaia <beamaia.work@gmail.com>
2023-07-03 21:12:04 -0300

    add beach day ordered

--- delete_topics.sh
+++ delete_topics.sh
@@ -4,3 +4,6 @@ echo "Deleting topics..."
 /kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic precipitationProbability  
 /kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachDay  
 /kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachHour
+
+/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachHourFiltered
+/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachDayOrdered
