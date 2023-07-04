9ffbabb1606fda1ab4f37e61162830a64b7e3534
beamaia <beamaia.work@gmail.com>
2023-07-04 15:15:16 -0300

    fix sunHour and change beachDayOrdered to beachDayGrouped

--- create_topics.sh
+++ create_topics.sh
@@ -4,9 +4,10 @@ echo "Creating topics..."
 /kafka/bin/kafka-topics.sh --create --topic precipitationProbability --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
 /kafka/bin/kafka-topics.sh --create --topic beachHour --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
 /kafka/bin/kafka-topics.sh --create --topic beachDay --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
+
 /kafka/bin/kafka-topics.sh --create --topic sunset --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
 /kafka/bin/kafka-topics.sh --create --topic sunrise --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
-/kafka/bin/kafka-topics.sh --create --topic sunHour --partitions 3 --replication-factor 1 --bootstrap-server kafka:
+/kafka/bin/kafka-topics.sh --create --topic sunHour --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
 
 /kafka/bin/kafka-topics.sh --create --topic beachHourFiltered --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
-/kafka/bin/kafka-topics.sh --create --topic beachDayOrdered --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
+/kafka/bin/kafka-topics.sh --create --topic beachDayGrouped --partitions 3 --replication-factor 1 --bootstrap-server kafka:9092
--- delete_topics.sh
+++ delete_topics.sh
@@ -5,5 +5,9 @@ echo "Deleting topics..."
 /kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachDay  
 /kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachHour
 
+/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic sunset
+/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic sunrise
+/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic sunHour
+
 /kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachHourFiltered
-/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachDayOrdered
+/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092 --delete --topic beachDayGrouped
